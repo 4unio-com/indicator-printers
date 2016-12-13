4834c5774f25d8c0a346f1f7c120cbc3ec42bd73
Rodney Dawes <rodney.dawes@canonical.com>
2016-12-13 13:56:51 -0500

    Add initial structs for Printer and Job types.

---
+++ src/job.h
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2016 Canonical Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 3, as published
+ * by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranties of
+ * MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR
+ * PURPOSE.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+namespace unity {
+namespace indicator {
+namespace printers {
+
+    struct Job {
+        // State to match ipp_jstate_t from cups.h
+        typedef enum {
+            PENDING = 3,
+            HELD,
+            PROCESSING,
+            STOPPED,
+            CANCELED,
+            ABORTED,
+            COMPLETED
+        } State;
+        State state = QUEUED;
+
+        unsigned int id;
+        std::string name;
+        std::string state_reasons;
+        unsigned int impressions_completed;
+    };
+
+} // printers
+} // indicators
+} // unity
